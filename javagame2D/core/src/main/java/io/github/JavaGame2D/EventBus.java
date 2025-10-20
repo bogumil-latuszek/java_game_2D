96f769595d2aff5dbf7f89a4f637197b217af82d
bogumil <bogumil_latuszek@o2.pl>
2025-10-20 19:43:35 +0200

    added crucial functionality to EventBus: routing events by utilizing generic functions and Consumer Interface

--- javagame2D/core/src/main/java/io/github/JavaGame2D/EventBus.java
+++ javagame2D/core/src/main/java/io/github/JavaGame2D/EventBus.java
@@ -1,11 +1,20 @@
 package io.github.JavaGame2D;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.function.Consumer;
+
 public class EventBus {
+    // Singleton (global object)
     private static EventBus singleInstance;
+
+    // Key: Event type, Value: List of Consumer functions
+    private final HashMap< Class<?>, ArrayList<Consumer> > subscribers = new HashMap<>();
+
     private EventBus(){
     }
 
-    //Singleton (global object)
+    // returns Singleton
     synchronized public static EventBus getInstance(){
         if(singleInstance == null){
             singleInstance = new EventBus();
@@ -13,6 +22,22 @@ public class EventBus {
         return singleInstance;
     }
 
+    // Subscribe: pass the event class and your function
+    // <T> is a generic type parameter - it means the method can work with any type: String, Integer, Double, etc.
+    public <T> void subscribe(Class<T> eventType, Consumer<T> handler) {
+        // if Event not registered as key, register it with new empty array as value
+        subscribers.computeIfAbsent(eventType, k -> new ArrayList<>());
+        // then add handler to that list
+        subscribers.get(eventType).add(handler);
+    }
 
-
+    // Publish: call all subscribed functions
+    public <T> void publish(T event) {
+        ArrayList<Consumer> handlers = subscribers.get(event.getClass());
+        if (handlers != null) {
+            for (Consumer handler : handlers) {
+                handler.accept(event);  // Call the function!
+            }
+        }
+    }
 }

c4043549db26db9c4065f2ec442bdc0b289ca03a
bogumil <bogumil_latuszek@o2.pl>
2025-10-20 23:21:54 +0200

    created system for controling player based on events from input system (very primitive)

---
+++ javagame2D/core/src/main/java/io/github/JavaGame2D/Systems/PlayerCharacterController.java
@@ -0,0 +1,32 @@
+package io.github.JavaGame2D.Systems;
+
+import io.github.JavaGame2D.Components.TransformComponent;
+import io.github.JavaGame2D.Entity;
+import io.github.JavaGame2D.Enums.ComponentType;
+import io.github.JavaGame2D.EventBus;
+
+public class PlayerCharacterController {
+    private Entity playerCharacter;
+
+    public PlayerCharacterController(){
+        EventBus.getInstance().subscribe(InputSystem.PlayerAction.class, this::handlePlayerAction);
+    }
+
+    public void setPlayerCharacter(Entity newPlayerCharacter){
+        this.playerCharacter = newPlayerCharacter;
+    }
+
+    public void handlePlayerAction(InputSystem.PlayerAction event){
+        TransformComponent transform = (TransformComponent) playerCharacter.getComponent(ComponentType.TRANSFORM);
+        switch (event.action){
+            case GO_LEFT:
+                transform.position.x -= 1;
+                break;
+            case GO_RIGHT:
+                transform.position.x += 1;
+                break;
+        }
+    }
+
+
+}

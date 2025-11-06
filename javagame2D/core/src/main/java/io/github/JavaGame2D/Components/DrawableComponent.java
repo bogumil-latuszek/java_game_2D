8e465b82722be4cc7342e84c7e019817c09dee34
bogumil <bogumil_latuszek@o2.pl>
2025-11-06 21:40:55 +0100

    removed getter/setter from DrawableComponent

--- javagame2D/core/src/main/java/io/github/JavaGame2D/Components/DrawableComponent.java
+++ javagame2D/core/src/main/java/io/github/JavaGame2D/Components/DrawableComponent.java
@@ -4,17 +4,9 @@ import com.badlogic.gdx.graphics.Texture;
 import io.github.JavaGame2D.Enums.ComponentType;
 
 public class DrawableComponent implements Component{
-    private Texture sprite;
+    public Texture sprite;
 
     public DrawableComponent(Texture sprite){
-        this.sprite = sprite;
-    }
-
-    public Texture getSprite(){
-        return  this.sprite;
-    }
-
-    public void setSprite(Texture sprite){
         this.sprite = sprite;
     }
 

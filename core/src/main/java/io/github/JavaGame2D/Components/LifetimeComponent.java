822330923b46e68930c7f0f6030f97c6783c656f
bogumil-latuszek <bogumil_latuszek@o2.pl>
2026-07-02 20:34:25 +0200

    added lifetime component
    Basically a timer for planned destruction. Useful for particles, projectiles, etc.

---
+++ core/src/main/java/io/github/JavaGame2D/Components/LifetimeComponent.java
@@ -0,0 +1,10 @@
+package io.github.JavaGame2D.Components;
+
+public class LifetimeComponent implements Component{
+    public float deathTimer = 1; // In Seconds
+
+    @Override
+    public long getSignature() {
+        return ComponentSignatures.LIFETIME;
+    }
+}

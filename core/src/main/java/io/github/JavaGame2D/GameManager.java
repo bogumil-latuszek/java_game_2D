57b8f66ae54c05f48f288bed2734dcefc4d4e3fa
bogumil-latuszek <bogumil_latuszek@o2.pl>
2026-07-10 22:53:50 +0200

    decoupling "LEVEL_EDIT" from "STANDARD_GAMEPLAY" mode. Created seperate loop for "STANDARD_GAMEPLAY" in PlayScreen.
    EditScreen will be added in near future. Why the main loop needed to change: switching from ApplicationAdapter to Game + Screen + Stage allows for separation of handling and rendering of "UI" and "the game world"

--- core/src/main/java/io/github/JavaGame2D/GameManager.java
+++ core/src/main/java/io/github/JavaGame2D/GameManager.java
@@ -1,27 +1,20 @@
 package io.github.JavaGame2D;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.Game;
 import io.github.JavaGame2D.Enums.GameMode;
 import io.github.JavaGame2D.Systems.*;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class GameManager extends ApplicationAdapter {
+public class GameManager extends Game {
 
     GameMode gameMode;
 
     RenderingSystem renderingSystem;
     PhysicsSystem physicsSystem;
-    UserInterface userInterface;
-    InputSystem inputSystem;
-    PlayerCharacterController playerCharacterController;
-    Long previousTimeframe;
     GameSettings gameSettings;
     LevelManager levelManager;
     EntityComponentManager entityComponentManager;
-    AnimationSystem animationSystem;
-    DamageSystem damageSystem;
-    PlayerDeathHandler playerDeathHandler;
+    FileSystem fileSystem;
 
     public GameManager(GameMode mode){
         gameMode = mode;
@@ -29,117 +22,27 @@ public class GameManager extends ApplicationAdapter {
     }
 
 
-
     @Override
     public void create() {
 
-       this.initializeSystems();
-
-//         LEVEL 1
-//        entityComponentManager.createPlatform(0f,    -2.5f, 5f,   1f);
-//        entityComponentManager.createPlatform(5f,    1.125f,2.5f, 2.5f);
-//        entityComponentManager.createPlatform(10f,   2.5f,  5f,   2.5f);
-//        entityComponentManager.createPlatform(12.5f, 2.5f,  1.25f,10f);
-//        entityComponentManager.createPlatform(17.5f, 2.5f,  1.25f,17.5f);
-//        entityComponentManager.createPlatform(17.5f, -10f,  12.5f,1.25f);
-//        entityComponentManager.createPlatform(-6f,   -5f,   30f,  0.5f);
-//        entityComponentManager.createSpikes(7f, -4f,  2f,2f);
-//        entityComponentManager.createSpikes(5f, -4f,  2f,2f);
-//        entityComponentManager.createSpikes(3f, -4f,  2f,2f);
-//        entityComponentManager.createSpikes(1f, -4f,  2f,2f);
-//        entityComponentManager.createDestructibleCrate(-7f, -4f, 2f,2f);
-//        entityComponentManager.createDestructibleCrate(-7f, -2f, 2f,2f);
-//        entityComponentManager.createDestructibleCrate(-9f, -4f, 2f,2f);
-//        entityComponentManager.createPlatform(20f,   -6f,   2.5f, 0.5f);
-//        entityComponentManager.createPlatform(22.5f, -2f,   2.5f, 0.5f);
-//        entityComponentManager.createPlatform(20f,   2f,    2.5f, 0.5f);
-//        entityComponentManager.createPlatform(22.5f, 6f,    2.5f, 0.5f);
-//        entityComponentManager.createPlatform(20f,   10f,   2.5f, 0.5f);
-//        entityComponentManager.createPlatform(22.5f, 14f,   2.5f, 0.5f);
-//        entityComponentManager.createPlatform(17.5f, 18f,   5f,   0.5f);
-//        entityComponentManager.createPlatform(17.5f, 22f,   2.5f, 0.5f);
-//        entityComponentManager.createPlatform(17.5f, 26f,   1.25f,0.5f);
-//        entityComponentManager.createTeleporter(17.5f, 27.5f, 2f,2f, 2);
-//        levelManager.saveLevel(1);
-
-//        // LEVEL 2
-//
-//        entityComponentManager.createPlatform(5f,2f,15f,10f);
-//        entityComponentManager.createPlatform(35f, 0f, 40f,20f);
-//        entityComponentManager.createPlatform(30f, 5f, 10f,16f);
-//        entityComponentManager.createPlatform(40f, 14.5f, 5f,5f);
-//        entityComponentManager.createPlatform(50f, 12.5f, 10f,15f);
-//        entityComponentManager.createPlatform(65f, 15f, 30f,15f);
-//        entityComponentManager.createPlatform(65f, 35f, 30f,15f);
-//        entityComponentManager.createPlatform(82.5f, 14f, 5f,15f);
-//        entityComponentManager.createPlatform(87.5f, 12.5f, 5f,15f);
-//        entityComponentManager.createPlatform(92.5f, 13.5f, 5f,15f);
-//        entityComponentManager.createPlatform(97.5f, 12f, 5f,15f);
-//        entityComponentManager.createPlatform(87.5f, 35.5f, 5f,15f);
-//        entityComponentManager.createPlatform(97.5f, 32f, 5f,15f);
-//        entityComponentManager.createPlatform(107.5f, 18f, 5f,10f);
-//        entityComponentManager.createPlatform(112.5f, 37.5f, 5f,15f);
-//        entityComponentManager.createPlatform(117.5f, 20f, 3f,5f);
-//        entityComponentManager.createTeleporter(117.5f, 24f, 2f,2f, 3);
-//        levelManager.spawnPointIDtoPosition.put(0,new Vector2(5,10));
-//        levelManager.defaultSpawnPoint = 0;
-//        levelManager.saveLevel(2);
-
-//         LEVEL 3
-
-//        entityComponentManager.createPlatform(10f, 25f, 20f,5f);
-//        entityComponentManager.createPlatform(10f, 5f, 20f,5f);
-//        entityComponentManager.createPlatform(0f, 15f, 5f,30f);
-//        entityComponentManager.createPlatform(20f, 15f, 5f,30f);
-//        entityComponentManager.createPlatform(10f, 11f, 2f,0.5f);
-//        entityComponentManager.createBackgroundElement(10f, 15f, 10f,10f);
-//        levelManager.spawnPointIDtoPosition.put(0,new Vector2(10,16));
-//        levelManager.defaultSpawnPoint = 0;
-//        levelManager.saveLevel(3);
+        initializeSharedSystems();
 
         levelManager.loadLevel(1);
 
-//        int playerID = entityComponentManager.createPlayer();
-//        EventBus.getInstance().publish(new PlayerIDChanged(playerID));
-
-        previousTimeframe = System.currentTimeMillis();
+        setScreen(new PlayScreen(this));
     }
 
-    // this function may be called "render"
-    // but this is actually one frame of our main loop
-    @Override
-    public void render() {
-        Long currentTime = System.currentTimeMillis();
-        float deltaTimeInMilliseconds = (float)(currentTime - previousTimeframe);
-        // TODO: make delta time uses consistent across the system!
-        float deltaTimeInSeconds = deltaTimeInMilliseconds/1000;
-        previousTimeframe = currentTime;
 
-        inputSystem.update(deltaTimeInSeconds);
-        playerCharacterController.update(deltaTimeInSeconds);
-        userInterface.update(deltaTimeInMilliseconds);
+    private void initializeSharedSystems(){
+        gameSettings = new GameSettings();
+        gameSettings = gameSettings.loadSettings();
+        gameSettings.saveSettings();
 
-        damageSystem.update(deltaTimeInSeconds);
-        physicsSystem.update(deltaTimeInSeconds);
-        animationSystem.update(deltaTimeInMilliseconds);
+        fileSystem = new FileSystem();
+        entityComponentManager = new EntityComponentManager();
 
-        renderingSystem.render();
-        userInterface.render();
-
-        playerDeathHandler.update(deltaTimeInSeconds);
-
-        levelManager.loadLevelIfChanged();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        this.renderingSystem.resizeViewport(width, height);
-        System.out.println("Resize called: " + width + "x" + height); // Debug
-    }
-
-    @Override
-    public void dispose() {
-        renderingSystem.dispose();
-        userInterface.dispose();
+        renderingSystem = new RenderingSystem(entityComponentManager);
+        physicsSystem = new PhysicsSystem(entityComponentManager, gameSettings);
+        levelManager = new LevelManager(fileSystem,entityComponentManager);
     }
 }
---
+++ core/src/main/java/io/github/JavaGame2D/PlayScreen.java
@@ -0,0 +1,140 @@
+package io.github.JavaGame2D;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import io.github.JavaGame2D.Enums.GameMode;
+import io.github.JavaGame2D.Systems.*;
+
+public class PlayScreen implements Screen {
+
+    private final GameManager gameManager;
+    private Stage inGameHUD; // handles and draws Hp bar, mini-menu, etc.
+    private Stage gameOverlay; // handles and draws menu, dialogue, inventory, etc.
+    private boolean isPaused = false;
+
+    private RenderingSystem renderingSystem;
+    private PhysicsSystem physicsSystem;
+    private GameSettings gameSettings;
+    private LevelManager levelManager;
+    private EntityComponentManager entityComponentManager;
+    private FileSystem fileSystem;
+
+    private PlayerInterface playerInterface;
+    private InputSystem inputSystem;
+    private PlayerCharacterController playerCharacterController;
+    private AnimationSystem animationSystem;
+    private DamageSystem damageSystem;
+    private PlayerDeathHandler playerDeathHandler;
+    private TeleporterSystem teleporterSystem;
+
+
+    public PlayScreen(GameManager gameManager){
+        this.gameManager = gameManager;
+
+        // get shared systems
+        renderingSystem = gameManager.renderingSystem;
+        physicsSystem = gameManager.physicsSystem;
+        gameSettings = gameManager.gameSettings;
+        levelManager = gameManager.levelManager;
+        entityComponentManager = gameManager.entityComponentManager;
+        fileSystem = gameManager.fileSystem;
+
+        // create HUD stage
+        inGameHUD = new Stage(new ScreenViewport());
+        // ... add your health bar, score labels ...
+
+        // create HUD stage
+        gameOverlay = new Stage(new ScreenViewport());
+        // ... add your menu, inventory, dialogue...
+
+        // Instantiate play-only systems
+        playerInterface = new PlayerInterface();
+        playerCharacterController = new PlayerCharacterController(entityComponentManager);
+        animationSystem = new AnimationSystem(entityComponentManager, fileSystem, playerCharacterController);
+        inputSystem = new InputSystem();
+        damageSystem = new DamageSystem(entityComponentManager);
+        playerDeathHandler = new PlayerDeathHandler();
+        teleporterSystem = new TeleporterSystem(entityComponentManager);
+
+        levelManager.loadLevel(1);
+
+    }
+
+    @Override
+    public void show() {
+        // Set input processor (UI first, then game controls)
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(gameOverlay);
+        multiplexer.addProcessor(inGameHUD);
+        //multiplexer.addProcessor(gameInputProcessor);// since game doesn't eat the same input as overlay/hud this isn't important FOR NOW
+        Gdx.input.setInputProcessor(multiplexer);
+    }
+
+    @Override
+    public void render(float delta) {
+        if (!isPaused){
+            // update Game World
+            inputSystem.update(delta);
+            playerCharacterController.update(delta);
+            playerInterface.update(delta);
+
+            damageSystem.update(delta);
+            physicsSystem.update(delta);
+            animationSystem.update(delta);
+
+            playerDeathHandler.update(delta);
+        }
+        // Render Game World
+        renderingSystem.render(GameMode.STANDARD_GAMEPLAY);
+        playerInterface.render();
+
+        // Render HUD
+        inGameHUD.act(delta);
+        inGameHUD.draw();
+
+        // render overlay
+        gameOverlay.act(delta);
+        gameOverlay.draw();
+
+        // Load new level if changed
+        levelManager.loadLevelIfChanged();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        inGameHUD.getViewport().update(width, height, true);
+        gameOverlay.getViewport().update(width, height, true);
+        this.renderingSystem.resizeViewport(width, height); //Should it be "this"? I sense a discrepency between Screen and GameManager
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        // CRITICAL: Remove play-only systems so they don't run in EditMode
+        //engine.removeSystem(playerControl);
+        //engine.removeSystem(enemyAI);
+        Gdx.input.setInputProcessor(null); // prevent input leaks
+    }
+
+    @Override
+    public void dispose() {
+        //TODO: check why this is done
+        inGameHUD.dispose();
+        gameOverlay.dispose();
+
+        renderingSystem.dispose();
+        playerInterface.dispose();
+    }
+}

3f425cef1c497087dc5ef90f6965713ce56c2e63
bogumil-latuszek <bogumil_latuszek@o2.pl>
2026-09-24 09:04:54 +0200

    created PrefabInstance to store data of an entity with PrefabComponent

---
+++ core/src/main/java/io/github/JavaGame2D/SaveData/PrefabInstance.java
@@ -0,0 +1,9 @@
+package io.github.JavaGame2D.SaveData;
+
+import java.util.HashMap;
+
+public class PrefabInstance {
+    public int entityID;
+    public String prefabName;
+    public HashMap<String, Object> overrides;
+}
